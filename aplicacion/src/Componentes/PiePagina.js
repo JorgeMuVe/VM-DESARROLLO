b8ab27b0571a48c7500bf51b96bba17debdd9407
Jorge Muñiz <jkmvz5@gmail.com>
2020-05-08 14:54:10 -0500

    Mayo 05 14:54

--- aplicacion/src/Componentes/PiePagina.js
+++ aplicacion/src/Componentes/PiePagina.js
@@ -29,17 +29,17 @@ export class Menu extends React.Component {
 
                     <div className="RedesSociales">
                         <h3>Siguenos en:</h3>
-                        <a href="#">
-                            <img className="facebook" src="/img/RedesSociales/facebook.png"></img>
+                        <a href="https://www.facebook.com/">
+                            <img className="facebook" src="/img/RedesSociales/facebook.png" alt="Icono Facebook"></img>
                         </a>
-                        <a href="#">
-                            <img className="ig" src="/img/RedesSociales/IG.png"></img>
+                        <a href="https://twitter.com/explore">
+                            <img className="ig" src="/img/RedesSociales/IG.png" alt="Icono Instagram"></img>
                         </a>
-                        <a href="#">
-                            <img className="twitter" src="/img/RedesSociales/twitter.png"></img>
+                        <a href="https://www.instagram.com/">
+                            <img className="twitter" src="/img/RedesSociales/twitter.png" alt="IconoTwitter"></img>
                         </a>
-                        <a href="#">
-                            <img className="wpp" src="/img/RedesSociales/wpp.png"></img>
+                        <a href="https://www.whatsapp.com/">
+                            <img className="wpp" src="/img/RedesSociales/wpp.png" alt="Icono Whatsapp"></img>
                         </a>
                     </div>
                 </div>
