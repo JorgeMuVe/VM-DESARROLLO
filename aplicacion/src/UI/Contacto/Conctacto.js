b6cde1f672fb1b3675083a095aa1550679b299b6
xavier <xaviervch@gmail.com>
2020-05-08 10:36:47 -0500

    contacto

--- aplicacion/src/UI/Contacto/Conctacto.js
+++ aplicacion/src/UI/Contacto/Conctacto.js
@@ -1,5 +1,5 @@
 /*
--- Description:      ERROR EN ENCONTRAR RUTA TIEDA VIRTUAL
+-- Description:      Pagina de Contacto
 -- @Copyright        Jorge.Muvez - World Connect Perú - 2020-00-00
 */
 
@@ -9,7 +9,7 @@ import React from 'react';
 /* VARIABLES GLOBALES */
 const estadoInicial = {};
 
-export class Error404 extends React.Component {
+export class Contacto extends React.Component {
     constructor(props) {
         super(props);
         this.state = estadoInicial;
@@ -17,128 +17,90 @@ export class Error404 extends React.Component {
     render() {
         return (
             <div className="Contacto">
-                <div>
+                <div className="faq">
                     <h2>PREGUNTAS FRECUENTES.</h2>
-                    <div class="pxlr-club-faq">
-                        <div class="content">
-                            <div class="panel-group" id="accordion" role="tablist"
-                                aria-multiselectable="true">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading" id="headingOne" role="tab">
-                                        <h4 class="panel-title">
-                                            <a class="collapsed" role="button" data-toggle="collapse"
-                                                data-parent="#accordion" href="#collapseOne"
-                                                aria-expanded="false" aria-controls="collapseOne">01.
-                                                Anonymous user won’t receive
-                                                            email if question is private? <i
-                                                    class="pull-right fa fa-plus"></i></a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-collapse collapse" id="collapseOne" role="tabpanel"
-                                        aria-labelledby="headingOne">
-                                        <div class="panel-body pxlr-faq-body">
-                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                            accusamus terry richardson ad squid. 3 wolf moon officia
-                                            aute,
-                                            non cupidatat skateboard dolor brunch. Food truck quinoa
-                                            nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                                            aliqua
-                                            put a bird on it squid single-origin coffee nulla assumenda
-                                            shoreditch et. Nihil anim keffiyeh helvetica, craft beer
-                                            labore
-                                            wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                                            excepteur butcher vice lomo. Leggings occaecat craft beer
-                                            farm-to-table, raw denim aesthetic synth nesciunt you
-                                            probably
-                                                            haven't heard of them accusamus labore sustainable VHS.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading" id="headingTwo" role="tab">
-                                        <h4 class="panel-title">
-                                            <a class="collapsed" role="button" data-toggle="collapse"
-                                                data-parent="#accordion" href="#collapseTwo"
-                                                aria-expanded="false" aria-controls="collapseTwo">02. How to
-                                                have
-                                                            an editor like this editor here for Q&A plugin? <i
-                                                    class="pull-right fa fa-plus"></i></a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-collapse collapse" id="collapseTwo" role="tabpanel"
-                                        aria-labelledby="headingTwo">
-                                        <div class="panel-body pxlr-faq-body">
-                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                            accusamus terry richardson ad squid. 3 wolf moon officia
-                                            aute,
-                                            non cupidatat skateboard dolor brunch. Food truck quinoa
-                                            nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                                            aliqua
-                                            put a bird on it squid single-origin coffee nulla assumenda
-                                            shoreditch et. Nihil anim keffiyeh helvetica, craft beer
-                                            labore
-                                            wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                                            excepteur butcher vice lomo. Leggings occaecat craft beer
-                                            farm-to-table, raw denim aesthetic synth nesciunt you
-                                            probably
-                                                            haven't heard of them accusamus labore sustainable VHS.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading" id="headingThree" role="tab">
-                                        <h4 class="panel-title"><a class="collapsed" role="button"
-                                            data-toggle="collapse"
-                                            data-parent="#accordion"
-                                            href="#collapseThree"
-                                            aria-expanded="false"
-                                            aria-controls="collapseThree">03. Problem
-                                                            with Knowledge base Article and knowledge base setting? <i
-                                                class="pull-right fa fa-plus"></i></a></h4>
-                                    </div>
-                                    <div class="panel-collapse collapse" id="collapseThree" role="tabpanel"
-                                        aria-labelledby="headingThree">
-                                        <div class="panel-body pxlr-faq-body">
-                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                            accusamus terry richardson ad squid. 3 wolf moon officia
-                                            aute,
-                                            non cupidatat skateboard dolor brunch. Food truck quinoa
-                                            nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                                            aliqua
-                                            put a bird on it squid single-origin coffee nulla assumenda
-                                            shoreditch et. Nihil anim keffiyeh helvetica, craft beer
-                                            labore
-                                            wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                                            excepteur butcher vice lomo. Leggings occaecat craft beer
-                                            farm-to-table, raw denim aesthetic synth nesciunt you
-                                            probably
-                                                            haven't heard of them accusamus labore sustainable VHS.</p>
-                                        </div>
-                                    </div>
-                                </div>
+                    <button class="accordion">Section 1</button>
+                    <div class="panel">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+
+                    <button class="accordion">Section 2</button>
+                    <div class="panel">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+
+                    <button class="accordion">Section 3</button>
+                    <div class="panel">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+
+                    {/* <script>
+                        var acc = document.getElementsByClassName("accordion");
+                        var i;
+
+                        for (i = 0; i < acc.length; {
+                            acc[i].addEventListener("click", function () {
+                                this.classList.toggle("active");
+                                var panel = this.nextElementSibling;
+                                if (panel.style.maxHeight) {
+                                    panel.style.maxHeight = null;
+                                } else {
+                                    panel.style.maxHeight = panel.scrollHeight + "px";
+                                }
+                            });
+                        }
+                    </script> */}
+                </div>
+
+                <div className="Contact">
+                    <div className="info_contacto">
+                        <h2>CONTÁCTENOS</h2>
+                        <div className="contacto1">
+                            <div>
+                                <span class="title">Celular</span>
+                                <strong>987654321</strong>
+                            </div>
+                            <div>
+                                <span class="title">Correo Electrónico</span>
+                                <strong>Correo@electronico.com</strong>
                             </div>
                         </div>
-                    </div>
-                </div>
-                
-                <div>
-                    <h2>CONTÁCTENOS</h2>
-                    <div className="Contacto1">
-                        <div>
-                            <span class="title">Celular</span>
-                            <strong>987654321</strong>
-                        </div>
-                        <div>
-                            <span class="title">Correo Electrónico</span>
-                            <strong>Correo@electronico.com</strong>
+                        <div className="direccion">
+                            <span class="title">Dirección</span>
+                            <strong>121, Calle Los Girasoles, Victoria 3000 Cusco</strong>
                         </div>
                     </div>
-                    <div className="direccion">
-                        <span class="title">Dirección</span>
-                        <strong>121, Calle Los Girasoles, Victoria 3000 Cusco</strong>
-                    </div>
-                    <div>
-                        <h2>Get In Touch</h2>
+
+                    <div className="formulario">
+                        <h2>Formulario de Contacto</h2>
+                        <p style={{ marginBottom: '-10px' }}>Te invitamos a contactarnos, te responderemos a la brevedad.</p>
+                        <p className="obligatorio">*Todos los campos son obligatorios</p>
+                        <form className="formulario1" action="">
+                            <div className="info">
+                                <label for="nombres">Nombres *</label>
+                                <input type="text" name="nombres" id="nombres" required></input>
+                            </div>
+                            <div className="info">
+                                <label for="apellidos">Apellidos *</label>
+                                <input type="text" name="apellidos" id="apellidos" required></input>
+                            </div>
+                            <div className="info">
+                                <label for="email">email:</label>
+                                <input type="email" name="email" id="email" required></input>
+                            </div>
+                            <div className="info">
+                                <label for="telefono">telefono:</label>
+                                <input type="text" name="telefono" id="telefono"></input>
+                            </div>
+                            <div className="mensaje">
+                                <label for="mensaje">mensaje:</label>
+                                <textarea rows="6" id="mensaje" name="mensaje" required></textarea>
+                            </div>
+                            <div>
+                                <button type="submit">Enviar</button>
+                            </div>
+                        </form>
+
                     </div>
                 </div>
             </div >
@@ -146,4 +108,4 @@ export class Error404 extends React.Component {
     }
 }
 
-export default Error404;
+export default Contacto;
